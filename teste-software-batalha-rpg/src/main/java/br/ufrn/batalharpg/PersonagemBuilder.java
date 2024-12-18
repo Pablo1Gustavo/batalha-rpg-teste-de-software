ca2e527549ecfa49befd855b1bf928a7e3274127
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2024-12-18 00:47:37 -0300

    refactor: refatora PersonagemBuilder

--- teste-software-batalha-rpg/src/main/java/br/ufrn/batalharpg/PersonagemBuilder.java
+++ teste-software-batalha-rpg/src/main/java/br/ufrn/batalharpg/PersonagemBuilder.java
@@ -1,50 +1,46 @@
 package br.ufrn.batalharpg;
 
-public class PersonagemBuilder {
+public class PersonagemBuilder
+{
+    private Integer ataque, defesa, velocidade, resistencia;
 
-    private Integer ataque;
-
-    private Integer defesa;
-
-    private Integer velocidade;
-
-    private Integer resistencia;
-
-    public static PersonagemBuilder construaUmPersonagem() {
+    public static PersonagemBuilder criar()
+    {
         return new PersonagemBuilder();
     }
 
-    public PersonagemBuilder comAtaque(Integer ataque) {
+    public PersonagemBuilder comAtaque(Integer ataque)
+    {
         this.ataque = ataque;
         return this;
     }
 
-    public PersonagemBuilder comDefesa(Integer defesa) {
+    public PersonagemBuilder comDefesa(Integer defesa)
+    {
         this.defesa = defesa;
         return this;
     }
 
-    public PersonagemBuilder comVelocidade(Integer velocidade) {
+    public PersonagemBuilder comVelocidade(Integer velocidade)
+    {
         this.velocidade = velocidade;
         return this;
     }
 
-    public PersonagemBuilder comResistencia(Integer resistencia) {
+    public PersonagemBuilder comResistencia(Integer resistencia)
+    {
         this.resistencia = resistencia;
         return this;
     }
 
-    public Assassino doTipoAssassino() {
+    public Assassino doTipoAssassino()
+    {
         return new Assassino(ataque, defesa, velocidade, resistencia);
     }
 
-    public Guerreiro doTipoGuerreiro() {
+    public Guerreiro doTipoGuerreiro()
+    {
         return new Guerreiro(ataque, defesa, velocidade, resistencia);
     }
-
-    public Assassino umAssassinoValido() {
-        return new Assassino(7, 3, 7, 3);
-    }
-
 }
 
